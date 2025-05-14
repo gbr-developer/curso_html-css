343a46798b3073214f2879415d2505e5352ccbfd
Gabriel Borges Reinaldo <65171922+gbr-developer@users.noreply.github.com>
2025-05-14 09:29:23 -0300

    Adicionei Módulo 4
    Projeto login em construção

---
+++ modulo-4/19_formularios/cadastro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <?php 
+        echo "<p>Você se cadastrou com sucesso! </p><br>";
+        echo "<p>Aprenda PHP para seguir apartir daqui</p>"
+    ?>
+</body>
+</html>
